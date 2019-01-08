22c9914b8ed0cad2cf4d7d4858465e58b8b920dc
Changsik00 <lowmans00@gmail.com>
2019-01-08 23:24:46 +0900

    urls path test > add index2

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,4 +1,7 @@
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse("Hello, world. You're at the polls index.1")
+
+def index2(request):
+    return HttpResponse("Hello, world. You're at the polls index2.")    
