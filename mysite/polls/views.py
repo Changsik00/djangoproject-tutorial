ae548ea7403d3d4a0d2e7f9b84dbf0484254d5e7
Dennis <dennis@genoplan.com>
2019-01-09 16:04:59 +0900

    404 Error , Template , URL Namespace

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,8 +1,21 @@
 from django.http import HttpResponse
+from django.template import loader
+
+# https://docs.djangoproject.com/ko/2.1/intro/tutorial03/
+from django.shortcuts import get_object_or_404, render
+from django.http import Http404
+
 from .models import Question
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.1")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    # template = loader.get_template('polls/index.html')
+    # return HttpResponse(template.render(context, request))
+    return render(request, 'polls/index.html', context)
+
 
 def index2(request):
     return HttpResponse("Hello, world. You're at the polls index2.")    
@@ -12,8 +25,15 @@ def index3(request):
     output = ', '.join([q.question_text for q in latest_question_list])
     return HttpResponse(output)
 
+
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    # try:
+    #     question = Question.objects.get(pk=question_id)
+    # except Question.DoesNotExist:
+    #     raise Http404("Question does not exist")
+
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
